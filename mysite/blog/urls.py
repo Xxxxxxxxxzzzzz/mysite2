ce6fbf63705f12fc7069918a36bbcb3eec925202
HP <838956316@qq.com>
2019-07-06 18:01:49 +0800

    添加了博客文章查看功能的路径

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('',views.blog_title), #第一个参数为空，即访问应用的根：127.0.0.1:8000/blog。第二个参数为响应这个请求的函数。
-    path('<int:article_id>/', views.blog_article),
+    path('', views.blog_title, name='blog_title'), #第一个参数为空，即访问应用的根：127.0.0.1:8000/blog。第二个参数为响应这个请求的函数。
+    path('<int:article_id>/', views.blog_article, name='blog_article'),
 ]
